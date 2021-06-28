cbb77d998c56452d1fbaf75a7a460272ef5d009f
cmstroe <cristina.stroe98@yahoo.com>
2021-06-28 16:27:30 +0200

    add funding class

---
+++ src/funding_task_preprocessor.py
@@ -0,0 +1,50 @@
+from transformers import DataProcessor, InputExample
+from preprocessors import TextClassificationProcessor, processors_mapping, num_labels_mapping, output_modes_mapping, compute_metrics_mapping
+
+
+class FundingClassification(TextClassificationProcessor):
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training, dev and test sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            guid = "%s-%s" % (set_type, i)
+            if self.task_name == "ag_news":
+                examples.append(InputExample(guid=guid, text_a=line[1] + '. ' + line[2], short_text=line[1] + ".", label=line[0]))
+            elif self.task_name == "yelp_review_full":
+                examples.append(InputExample(guid=guid, text_a=line[1], short_text=line[1], label=line[0]))
+            elif self.task_name == "yahoo_answers":
+                text = line[1]
+                if not pd.isna(line[2]):
+                    text += ' ' + line[2]
+                if not pd.isna(line[3]):
+                    text += ' ' + line[3]
+                examples.append(InputExample(guid=guid, text_a=text, short_text=line[1], label=line[0])) 
+            elif self.task_name in ['mr', 'sst-5', 'subj', 'trec', 'cr', 'mpqa', 'funding']:
+                examples.append(InputExample(guid=guid, text_a=line[1], label=line[0]))
+            else:
+                raise Exception("Task_name not supported.")
+
+        return examples
+
+    def get_labels(self):
+        """See base class."""
+        if self.task_name == "mr":
+            return list(range(2))
+        elif self.task_name == "sst-5":
+            return list(range(5))
+        elif self.task_name == "subj":
+            return list(range(2))
+        elif self.task_name == "trec":
+            return list(range(6))
+        elif self.task_name == "cr":
+            return list(range(2))
+        elif self.task_name == "mpqa":
+            return list(range(2))
+        elif self.task_name == "funding":
+            return list(range(2))
+        else:
+            raise Exception("task_name not supported.")
+
+
+
+processors_mapping['funding'] = TextClassificationProcessor
