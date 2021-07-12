e1b8a89b49390bf9cc8a31656c4f037a8febbc39
cmstroe <cristina.stroe98@yahoo.com>
2021-07-12 14:28:29 +0300

    inference job

--- inference.py
+++ inference.py
@@ -1,8 +1,9 @@
 import torch
 from src.trainer import Trainer
 from src.models import BertForPromptFinetuning, RobertaForPromptFinetuning, resize_token_type_embeddings
+from transformers import RobertaConfig
 
 model_fn = RobertaForPromptFinetuning
-model_fn.from_pretrained('roberta-large', config = "result/partnership-prompt-demo-16-13-roberta-large-27549/config.json" , state_dict = "result/partnership-prompt-demo-16-13-roberta-large-27549/pytorch_model.bin")
+model_fn.from_pretrained('roberta-large', config = RobertaConfig.from_json_file("result/partnership-prompt-demo-16-13-roberta-large-27549/config.json") , state_dict = torch.load("result/partnership-prompt-demo-16-13-roberta-large-27549/pytorch_model.bin"))
 
 model.evaluate()
