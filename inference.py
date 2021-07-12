3738c11e4be601da6af6227f26f5a9b98358e9b6
cmstroe <cristina.stroe98@yahoo.com>
2021-07-12 14:21:29 +0300

    inference job

--- inference.py
+++ inference.py
@@ -3,6 +3,6 @@ from src.trainer import Trainer
 from src.models import BertForPromptFinetuning, RobertaForPromptFinetuning, resize_token_type_embeddings
 
 model_fn = RobertaForPromptFinetuning
-model.from_pretrained(config = "result/partnership-prompt-demo-16-13-roberta-large-27549/config.json" , state_dict = "result/partnership-prompt-demo-16-13-roberta-large-27549/pytorch_model.bin")
+model_fn.from_pretrained(config = "result/partnership-prompt-demo-16-13-roberta-large-27549/config.json" , state_dict = "result/partnership-prompt-demo-16-13-roberta-large-27549/pytorch_model.bin")
 
 model.evaluate()
