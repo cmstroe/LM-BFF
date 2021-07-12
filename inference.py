f014c16c7c42303bdae28c542ac6ea6be1aa3214
cmstroe <cristina.stroe98@yahoo.com>
2021-07-12 16:12:55 +0300

    inference job

--- inference.py
+++ inference.py
@@ -5,29 +5,257 @@ from src.models import BertForPromptFinetuning, RobertaForPromptFinetuning, resi
 from transformers import RobertaConfig
 from transformers import HfArgumentParser, TrainingArguments
 
-model_fn = RobertaForPromptFinetuning
-model_fn.from_pretrained('roberta-large', config = RobertaConfig.from_json_file("result/partnership-prompt-demo-16-13-roberta-large-27549/config.json") , state_dict = torch.load("result/partnership-prompt-demo-16-13-roberta-large-27549/pytorch_model.bin"))
-
-special_tokens = []
-
-# tokenizer = AutoTokenizer.from_pretrained(
-#         model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
-#         additional_special_tokens=special_tokens,
-#         cache_dir= ".",
-    # )
-
-# parser = HfArgumentParser((ModelArguments, DynamicDataTrainingArguments, TrainingArguments))
-
-trainer = Trainer(
-        model=model_fn,
-        args=None,
-        train_dataset=None,
-        eval_dataset="inference_data.csv",
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
+    """
+    model_name_or_path: str = field(
+        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
     )
-trainer.predict("inference_data.csv")
-# model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+    config_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+    )
+    cache_dir: Optional[str] = field(
+        default=None, metadata={"help": "Where do you want to store the pretrained models downloaded from s3"}
+    )
+    # Few-shot type
+    #   - finetune: standard fine-tuning
+    #   - prompt: prompt-based fine-tuning
+    #   - prompt-demo: prompt-based fine-tuning with demonstrations
+    few_shot_type: str = field(
+        default='prompt-demo',
+        metadata={"help": "Few-shot learning model type. Choice: finetune, prompt, prompt-demo"}
+    )
 
-# dataset = FewShotDataset(data_args, tokenizer=tokenizer, mode="test", use_demo=True)
-# eval_dataset = 
-# trainer.evaluate()
-# model_fn.evaluate()
+    # Only for BERT-type model
+    random_segment: bool = field(
+        default=False,
+        metadata={"help": "Whether to reinitialize the token type embeddings (only for BERT)."}
+    )
+
+@dataclass
+class DynamicDataTrainingArguments(DataTrainingArguments):
+    """
+    Arguments for dynamic training.
+    """
+    num_k: Optional[int] = field(
+        default=16,
+        metadata={"help": "Number of training instances per class"}
+    )
+
+    num_sample: Optional[int] = field(
+        default=16,
+        metadata={"help": "Number of samples (for inference) in fine-tuning with demonstrations"}
+    )
+
+    num_demo: Optional[int] = field(
+        default=1,
+        metadata={"help": "Number of demonstrations from each class"}
+    )
+
+    auto_demo: bool = field(
+        default=True,
+        metadata={"help": "Automatically generate template for using demonstrations"}
+    )
+
+    # For prompting
+    template: str = field(
+        default=None,
+        metadata={"help": "Template"}
+    )
+
+    mapping: str = field(
+        default=None,
+        metadata={"help": "Label word mapping"}
+    )
+
+    template_path: str = field(
+        default=None,
+        metadata={"help": "Path to a txt file that stores all the templates, one per line. Do not set this when prompt_path is used"}
+    )
+
+    mapping_path: str = field(
+        default=None,
+        metadata={"help": "Path to a txt file that stores all the label word mappings, one per line. Do not set this when prompt_path is used"}
+    )
+
+    prompt_path: str = field(
+        default=None,
+        metadata={"help": "Path to a txt file that stores all the prompts (templates and mappings), one per line"}
+    )
+ 
+    template_id: int = field(
+        default=None,
+        metadata={"help": "Template id if using template_path"}
+    )
+
+    mapping_id: int = field(
+        default=None,
+        metadata={"help": "Mapping id if using template_path"}
+    )
+
+    prompt_id: int = field(
+        default=None,
+        metadata={"help": "Prompt id if using prompt_path"}
+    )
+
+    top_n_template: int = field(
+        default=5,
+        metadata={"help": "Use top-n template in the template path"}
+    )
+
+    # For logging
+    tag: str = field(
+        default='',
+        metadata={"help": "Set the tag and find the result easier in the log."}
+    )
+
+    # For filtering when using demonstrations
+    demo_filter: bool = field(
+        default=False,
+        metadata={"help": "Only use similar instances in demonstrations"}
+    )
+
+    demo_filter_rate: float = field(
+        default=0.5,
+        metadata={"help": "Only use top-x\% similar instances in demonstrations"}
+    )
+
+    demo_filter_model: str = field(
+        default=None,
+        metadata={"help": "Model name for demonstration filter embeddings. Will load embeddings based on the model name."}
+    )
+
+    debug_mode: bool = field(
+        default=False,
+        metadata={"help": "Debug mode"}
+    )
+
+    # For max length
+    double_demo: bool = field(
+        default=False,
+        metadata={"help": "Use double length for using demonstrations"}
+    )
+
+    first_sent_limit: int = field(
+        default=None,
+        metadata={"help": "Limit the length of the first sentence (i.e., sent_0)"}
+    )
+
+    other_sent_limit: int = field(
+        default=None,
+        metadata={"help": "Limit the length of sentences other than the first sentence"}
+    )
+
+    use_full_length: bool = field(
+        default=None,
+        metadata={"help": "Use the full length (512)"}
+    )
+
+    # GPT-3's in-context learning
+    gpt3_in_context_head: bool = field(
+        default=False,
+        metadata={"help": "GPT-3's in-context learning (context at the beginning)"}
+    )
+
+    gpt3_in_context_tail: bool = field(
+        default=False,
+        metadata={"help": "GPT-3's in-context learning (context at the end)"}
+    )
+
+    gpt3_in_context_num: int = field(
+        default=32,
+        metadata={"help": "Number of context examples"}
+    )
+
+    truncate_head: bool = field(
+        default=False,
+        metadata={"help": "When exceeding the maximum length, truncate the head instead of the tail."}
+    )
+
+    # Do not set up the following fields. They are set up automatically.
+    prompt: bool = field(
+        default=False,
+        metadata={"help": "Whether to use prompt-based fine-tuning"}
+    )
+    template_list: list = field(
+        default=None,
+        metadata={"help": "(DO NOT List of templates (only initialized after the program starts."}
+    )
+
+
+@dataclass
+class DynamicTrainingArguments(TrainingArguments):
+    # For ensemble
+    array_id: int = field(
+        default=-1,
+        metadata={"help": "Array ID (contains seed and hyper-paramter search) to idenfity the model"}
+    )
+
+    model_id: int = field(
+        default=-1,
+        metadata={"help": "Model ID (contains template information) to identify the model"}
+    )
+
+    save_logit: bool = field(
+        default=False,
+        metadata={"help": "Save test file logit with name $TASK-$MODEL_ID-$ARRAY_ID.npy"}
+    )
+
+    save_logit_dir: str = field(
+        default=None,
+        metadata={"help": "Where to save the prediction result"}
+    )
+
+    # Regularization
+    fix_layers: int = field(
+        default=0,
+        metadata={"help": "Fix bottom-n layers when optimizing"}
+    )
+
+    # Training
+    save_at_last: bool = field(
+        default=False,
+        metadata={"help": "Instead of saving the best (dev performance) checkpoint, save the last checkpoint"}
+    )
+
+    # Turn off train/test
+    no_train: bool = field(
+        default=False,
+        metadata={"help": "No training"}
+    )
+    no_predict: bool = field(
+        default=False,
+        metadata={"help": "No test"}
+    )
+
+
+
+def main():
+    parser = HfArgumentParser((ModelArguments, DynamicDataTrainingArguments, DynamicTrainingArguments))
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
+    model_fn = RobertaForPromptFinetuning
+    model_fn.from_pretrained('roberta-large', config = RobertaConfig.from_json_file("result/partnership-prompt-demo-16-13-roberta-large-27549/config.json") , state_dict = torch.load("result/partnership-prompt-demo-16-13-roberta-large-27549/pytorch_model.bin"))
+
+    special_tokens = []
+
+    tokenizer = AutoTokenizer.from_pretrained(
+            model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
+            additional_special_tokens=special_tokens,
+            cache_dir= ".",
+        )
+
+    trainer = Trainer(
+            model=model_fn,
+            args=training_args,
+            train_dataset=None,
+            eval_dataset="inference_data.csv",
+        )
+    trainer.predict("inference_data.csv")
+
+    dataset = FewShotDataset(data_args, tokenizer=tokenizer, mode="test", use_demo=True)
