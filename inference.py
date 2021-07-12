e486dd526a003f6a6fd5b12913f2603d034f0f4a
cmstroe <cristina.stroe98@yahoo.com>
2021-07-12 17:34:37 +0300

    inference job

--- inference.py
+++ inference.py
@@ -290,11 +290,6 @@ def main():
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
 
-
-    train_dataset = (
-        FewShotDataset(data_args, tokenizer=tokenizer, mode="train", use_demo=("demo" in model_args.few_shot_type))
-    )
-
     model_fn = RobertaForPromptFinetuning
     model_fn = model_fn.from_pretrained('roberta-large', config = RobertaConfig.from_json_file("result/partnership-prompt-demo-16-13-roberta-large-27549/config.json") , state_dict = torch.load("result/partnership-prompt-demo-16-13-roberta-large-27549/pytorch_model.bin"))
 
@@ -304,6 +299,10 @@ def main():
             cache_dir= ".",
         )
     
+    train_dataset = (
+        FewShotDataset(data_args, tokenizer=tokenizer, mode="train", use_demo=("demo" in model_args.few_shot_type))
+    )
+
     model_fn.label_word_list = torch.tensor(train_dataset.label_word_list).long().cuda()
     special_tokens = []
 
