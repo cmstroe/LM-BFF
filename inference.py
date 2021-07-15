003c8838933813c00f3bc7b3f9f43e11862deba2
cmstroe <cristina.stroe98@yahoo.com>
2021-07-15 14:18:43 +0300

    prediction

--- inference.py
+++ inference.py
@@ -77,8 +77,8 @@ def main():
     df = pd.read_csv("inference_data.csv")
     device = torch.device('cuda')
     model_fn.label_word_list = ['yes','no']
-    model.data_args = data_args
-    model.model_args = model_args
+    model_fn.data_args = data_args
+    model_fn.model_args = model_args
     model_fn.to(device)
 
     for idx, row in df.iterrows():
