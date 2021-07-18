c4de193489ff670e4a6094a2834b0c64e3ef5187
cmstroe <cristina.stroe98@yahoo.com>
2021-07-19 00:48:04 +0300

    ing funding

--- inference.py
+++ inference.py
@@ -117,7 +117,7 @@ def main():
 
         df_results = df_results.append({"sentence" : row.sentence ,
                     "token_values" : torch.topk(logit, 1) ,
-                    "word" : tokenizer.decode(torch.argmax(logit))
+                    "word" : tokenizer.decode([torch.argmax(logit)])
                     },
                     ignore_index = True)
         torch.cuda.empty_cache()
