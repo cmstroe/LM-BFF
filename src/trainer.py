e101c9d28b773fcba3ce4257395b2b8de443e31b
cmstroe <cristina.stroe98@yahoo.com>
2021-07-20 00:56:03 +0300

    revert

--- src/trainer.py
+++ src/trainer.py
@@ -1,3 +1,19 @@
+########## The following part is copied from Transformers' trainer (3.4.0) ########## 
+
+# coding=utf-8
+# Copyright 2020-present the HuggingFace Inc. team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
 The Trainer class, to easily train a 🤗 Transformers from scratch or finetune it on a new task.
 """
@@ -336,7 +352,7 @@ class Trainer(transformers.Trainer):
                 if steps_trained_in_current_epoch > 0:
                     steps_trained_in_current_epoch -= 1
                     continue
-                print(tr_loss)
+
                 tr_loss += self.training_step(model, inputs)
 
                 if (step + 1) % self.args.gradient_accumulation_steps == 0 or (
