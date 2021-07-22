6849a1fdd2ba8a98f30daaba86a1f1f1623108b2
cmstroe <cristina.stroe98@yahoo.com>
2021-07-22 20:56:58 +0300

    ing funding

--- inference.py
+++ inference.py
@@ -64,7 +64,7 @@ def main():
             additional_special_tokens=special_tokens, truncation=True,
             cache_dir= ".",
         )
-    df = pd.read_csv("inference_data_1.csv")
+    df = pd.read_csv("inference_data.csv")
     device = torch.device('cuda:0')
     model_fn.label_word_list = torch.LongTensor([0,1])
     model_fn.data_args = data_args
