726607ba223687cbfd1ffea1ba9355511b1b3228
cmstroe <cristina.stroe98@yahoo.com>
2021-07-22 20:20:07 +0300

    funding

--- inference.py
+++ inference.py
@@ -123,7 +123,7 @@ def main():
 
             df_results = df_results.append({"sentence" : row.sentence ,
                     "token_values" : torch.topk(logit, 1) ,
-                    "word" : tokenizer.decode([torch.argmax(logit)]) if torch.argmax(logit) else "nothing", ignore_index = True)
+                    "word" : tokenizer.decode([torch.argmax(logit)]) if torch.argmax(logit) else "nothing"}, ignore_index = True)
                     # "yes_value" :  ,  
                     # "no_value" :   ,
                     # },
